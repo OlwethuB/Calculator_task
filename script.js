a836ed97a4db0970cd6f4a8ff57bbd227d78354e
OlwethuB <130047153+OlwethuB@users.noreply.github.com>
2023-05-23 08:49:08 +0200

    script.js

--- script.js
+++ script.js
@@ -6,115 +6,31 @@
 //   let Btnop = event.target.id;
 //   document.getElementById("input").innerHTML = Btnop;
 // }
-class Calculator {
-  constructor(inputTextElement) {
-    this.inputTextElement =inputTextElement
-    this.clear()
-  }
-  clear() {
-    this.input = ''
-    this.operation = undefined
-  }
-  delete() {
-    this.input = this.input.toString().slice(0, -1)
-  }
-  appendNumber(number) {
-    if (numb === '.' && this.input.includes('.')) return
-    this.input = this.input.toString() + number.toString()
-  }
-  Operators(operation) {
-    if (this.input === '') {
-      this.compute()
-    }
-    this.operation = operation
-    this.input = ''
-  }
-  compute() {
-    let computation
-    const current = parseFloat(this.input)
-    if (isNaN(input)) return
-    switch (this.operation) {
-      case '+':
-        computation = prev + current
-        break
-      case '-':
-        computation = prev - current
-        break
-      case '*':
-        computation = prev * current
-        break
-      case '÷':
-        computation = prev / current
-        break
-      default:
-        return
-    }
-    this.input = computation
-    this.operation = undefined
-  }
-  calculationnum(number) {
-    const inputnum = number.toString()
-    const decimals = parseFloat(inputnum.split('.')[0])
-    const decnum = inputnum.split('.')[1]
-    let dDisplay
-    if (isNaN(decimals)) {
-      dDisplay = ''
-    } else {
-      dDisplay = decimals.toLocaleString('en', { maximumFractionDigits: 0 })
-    }
-    if (decimals != null) {
-      return `${dDisplay}.${decimals}`
-    } else {
-      return dDisplay
-    }
+
+let display = document.getElementById('input');
+let currentExpression = '';
+function getID(id) {
+  currentExpression += id;
+  display.innerText = currentExpression;
+}
+function getop(operator) {
+  currentExpression += operator;
+  display.innerText = currentExpression;
+}
+function clear() {
+  currentExpression = '';
+  display.innerText = '';
+}
+function del() {
+  currentExpression = currentExpression.slice(0, -1);
+  display.innerText = currentExpression;
+}
+function calculate() {
+  try {
+    const result = eval(currentExpression);
+    display.innerText = result;
+    currentExpression = result;
+  } catch (error) {
+    display.innerText = 'Error';
   }
 }
-
-updateDisplay(){
-this.inputTextElement.innerHTML =
-this.calculationnum(this.input)
-}
-
-
-
-const btnnum = document.querySelectorAll('[data-number]')
-const btnopr = document.querySelectorAll('[data-operation]')
-const eqls = document.querySelector('[data-equals]')
-const del = document.querySelector('[data-del]')
-const clear = document.querySelector('[data-clear]')
-const inputTextElement = document.querySelector('[data-input]')
-const calculator = new Calculator(inputTextElement)
-btnnum.forEach(button => {
-  button.addEventListener('click', () => {
-    calculator.appendNumber(button.innerText)
-    calculator.updateDisplay()
-  })
-})
-btnopr.forEach(button => {
-  button.addEventListener('click', () => {
-    calculator.Operations(button.innerText)
-    calculator.updateDisplay()
-  })
-})
-eqls.addEventListener('click', button => {
-  calculator.compute()
-  calculator.updateDisplay()
-})
-clear.addEventListener('click', button => {
-  calculator.clear()
-  calculator.updateDisplay()
-})
-del.addEventListener('click', button => {
-  calculator.delete()
-  calculator.updateDisplay()
-})
-
-
-
-
-
-
-
-
-
-
